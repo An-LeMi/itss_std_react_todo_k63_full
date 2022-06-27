ea5e7d0e6e4d296512729ee20663c8ebf47f4d3f
Le-MinhMinh7 <>
2022-06-27 02:36:44 +0000

    Update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -17,52 +17,52 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
-export default firebase;
+// const db = firebase.firestore();
+// export default firebase;
 
-// Todoコレクションの全documentを取得する：
-export const getFirebaseItems = async () => {
-  try {
-    const snapshot = await db
-      .collection("todos")
-      .get();
-    const items = snapshot.docs.map(
-      (doc) => ({ ...doc.data(), id: doc.id })
-    );
-    return items;
-  } catch (err) {
-    console.log(err);
-    return [];
-  }
-}
+// // Todoコレクションの全documentを取得する：
+// export const getFirebaseItems = async () => {
+//   try {
+//     const snapshot = await db
+//       .collection("todos")
+//       .get();
+//     const items = snapshot.docs.map(
+//       (doc) => ({ ...doc.data(), id: doc.id })
+//     );
+//     return items;
+//   } catch (err) {
+//     console.log(err);
+//     return [];
+//   }
+// }
 
-// 新しいTodoをFirestoreに保存する：
-export const addFirebaseItem = async (item) => {
-  try {
-    const todoRef = db.collection("todos");
-    await todoRef.add(item);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// // 新しいTodoをFirestoreに保存する：
+// export const addFirebaseItem = async (item) => {
+//   try {
+//     const todoRef = db.collection("todos");
+//     await todoRef.add(item);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
-// Todoの情報を更新する：
-export const updateFirebaseItem = async (item, id) => {
-  try {
-    const todoRef = db.collection("todos").doc(id);
-    await todoRef.update(item);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// // Todoの情報を更新する：
+// export const updateFirebaseItem = async (item, id) => {
+//   try {
+//     const todoRef = db.collection("todos").doc(id);
+//     await todoRef.update(item);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
-// 任意のTodoを削除する
-export const deleteFirebaseItem = async (item) => {
-  const todoRef = db.collection("todos").doc(item.id);
-  await todoRef.delete().then(function () {
-  }).catch(function (err) {
-    console.log(err);
-  });
-};
+// // 任意のTodoを削除する
+// export const deleteFirebaseItem = async (item) => {
+//   const todoRef = db.collection("todos").doc(item.id);
+//   await todoRef.delete().then(function () {
+//   }).catch(function (err) {
+//     console.log(err);
+//   });
+// };
 
 
