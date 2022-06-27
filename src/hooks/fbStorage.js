c44bee69a349854a1feb9eb3a5bd2e70bce6f221
Le-MinhMinh7 <>
2022-06-27 02:37:19 +0000

    Save Todo on Firebase

--- src/hooks/fbStorage.js
+++ src/hooks/fbStorage.js
@@ -1,45 +1,45 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-// import { addFirebaseItem, updateFirebaseItem, getFirebaseItems, deleteFirebaseItem } from "../lib/firebase";
+import { addFirebaseItem, updateFirebaseItem, getFirebaseItems, deleteFirebaseItem } from "../lib/firebase";
 
-// function useFbStorage() {
-//   const [items, setItems] = useState([]);
+function useFbStorage() {
+  const [items, setItems] = useState([]);
 
-//   useEffect(() => {
-//     getItems();
-//   }, [items]);
+  useEffect(() => {
+    getItems();
+  }, [items]);
 
-//   const getItems = async () => {
-//     const _items = await getFirebaseItems();
-//     setItems(_items);
-//   };
+  const getItems = async () => {
+    const _items = await getFirebaseItems();
+    setItems(_items);
+  };
 
-//   const addItem = async item => {
-//     const newItem = { text: item.text, done: item.done };
-//     await addFirebaseItem(newItem);
-//     setItems([...items, newItem]);
-//   };
+  const addItem = async item => {
+    const newItem = { text: item.text, done: item.done };
+    await addFirebaseItem(newItem);
+    setItems([...items, newItem]);
+  };
 
-//   const updateItem = async checked => {
-//     const changes = { done: !checked.done };
-//     await updateFirebaseItem(changes, checked.id);
-//     const newItems = items.map((item) => {
-//       if (item.id === checked.id) {
-//         item = { ...item, changes}
-//       }
-//       return item;
-//     })
-//     setItems(newItems);
-//   }
+  const updateItem = async checked => {
+    const changes = { done: !checked.done };
+    await updateFirebaseItem(changes, checked.id);
+    const newItems = items.map((item) => {
+      if (item.id === checked.id) {
+        item = { ...item, changes}
+      }
+      return item;
+    })
+    setItems(newItems);
+  }
 
-//   const clearItems = () => {
-//     items.map(item => {
-//       deleteFirebaseItem(item);
-//     })
-//     setItems([]);
-//   };
+  const clearItems = () => {
+    items.map(item => {
+      deleteFirebaseItem(item);
+    })
+    setItems([]);
+  };
 
-//   return [items, addItem, updateItem, clearItems];
-// }
+  return [items, addItem, updateItem, clearItems];
+}
 
-// export default useFbStorage; 
+export default useFbStorage; 
